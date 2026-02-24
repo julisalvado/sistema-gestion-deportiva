ef124f5b73d48b8dc4ef93c61e539aab2edc43b3
Lara Larghi <137651485+larghi@users.noreply.github.com>
2026-02-24 12:29:57 -0300

    Añadi que admin pueda cancelar partido, dejar ocmentairo y estadística

--- sistema_gestion_deportiva/src/main/java/com/gestionDeportiva/modulos/usuarios/modelo/Administrador.java
+++ sistema_gestion_deportiva/src/main/java/com/gestionDeportiva/modulos/usuarios/modelo/Administrador.java
@@ -1,5 +1,6 @@
 package com.gestionDeportiva.modulos.usuarios.modelo;
 
+import com.gestionDeportiva.modulos.partidos.modelo.Estadistica;
 import com.gestionDeportiva.modulos.partidos.modelo.Partido;
 
 public class Administrador extends Usuario{
@@ -18,7 +19,21 @@ public class Administrador extends Usuario{
         partido.getDeporte().notificar(mensaje);
 
         return partido;
-
-        
     }
+
+        public void cancelarPartido(Partido partido) {
+            if (partido == null){
+                throw new IllegalArgumentException("El partido no puede ser nulo");
+            }
+            partido.cancelarPorAdmin(this);
+        }
+
+    public void dejarComentario(Partido partido, String contenido) {
+        partido.agregarComentario(this, contenido);
+    }
+
+    public void registrarEstadistica(Partido partido, Estadistica estadistica) {
+        partido.registrarEstadistica(this, estadistica);
+    }
+
 }
